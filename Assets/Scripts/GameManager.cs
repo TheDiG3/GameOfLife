8e75290bf7e45040c19e23325d5d1de3b6ffeea4
Simone Di Gravio <altairjp@gmail.com>
2014-06-12 17:14:44 -0700

    - Initial Setup - Camera Movement - Scene Setup

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * This class handles the user interface as well as the creation of the game simulator needed to simulate a Game Of Life.
+ */
+public class GameManager : MonoBehaviour
+{
+    #region Public Members
+
+    /** The time in seconds for the simulation loop. */
+    public float simulation_time_ = 0.3F;
+
+    #endregion
+
+    #region Private Members
+
+    /** The simulator used to run the game of life. */
+    private GameSimulator game_simulator_;
+
+    #endregion
+
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Code for the user interface.
+    void OnGUI()
+    {
+
+    }
+}
---
+++ Assets/Scripts/GameSimulator.cs
@@ -0,0 +1,51 @@
+﻿/**
+ * This class stores the game of life data and handles the simulation process. 
+ */
+public class GameSimulator
+{
+
+    #region Public Members
+
+    public enum CELL_STATE
+    {
+        DEAD,
+        ALIVE
+    }
+
+    #endregion
+
+    #region Private Members
+
+    /** <summary> The grid containing the states of all the cells. </summary> */
+    private CELL_STATE[ , ] cell_grid_;
+
+    #endregion
+
+
+    #region Public Methods
+
+    /**
+     * <summary> Constructs the game simulator object with the given cell grid. </summary>
+     * <param name="_cell_grid"> The cell grid to use. </param>
+     */
+    public GameSimulator(CELL_STATE[,] _cell_grid)
+    {
+        cell_grid_ = _cell_grid;
+    }
+
+    /**
+     * Simulates one cycle of the game of life.
+     */
+    public void simulate()
+    {
+
+    }
+    
+    #endregion
+
+    #region Private Methods
+
+
+
+    #endregion
+}
